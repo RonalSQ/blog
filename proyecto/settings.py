648fcf9310fb0f68fb66d85ccc28530bf5c06ff9
ronal1606 <ronalsanchez.estudiante@iser.edu.co>
2026-05-04 22:12:37 -0500

    Fix empty state links and s3 storage headobject permission

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -111,7 +111,8 @@ if os.environ.get('SUPABASE_S3_ACCESS_KEY'):
     
     AWS_S3_REGION_NAME = 'us-east-1' # Normalmente us-east-1 o la de tu proyecto supabase
     AWS_S3_SIGNATURE_VERSION = 's3v4'
-    AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_FILE_OVERWRITE = True
+    AWS_DEFAULT_ACL = None
     
     STORAGES = {
         "default": {
