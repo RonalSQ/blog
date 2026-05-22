641993a5ec79d61fe1e9d980d917ffeaeaae1643
ronal sanchez <ronalsanchez36@gmail.com>
2026-05-21 21:34:31 -0500

    Permite configurar SMTP mediante variables de entorno

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -139,12 +139,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuración de Envío de Correos (Gmail SMTP)
 EMAIL_BACKEND = 'app.utils.AsynchronousEmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'tu_correo@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # YouTube Embed Referrer Policy Fix
 SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
